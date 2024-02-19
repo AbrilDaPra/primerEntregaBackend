3e428eaafe5efb372b62b86858580434ae029d42
Abril Da Pra <abydapra@gmail.com>
2024-02-19 14:44:05 -0300

    Creo carpetas src y database y archivos json. Comienzo con el archivo app.js creando las variables, middlewares y routes

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.listen(port, console.log("El servidor esta corriendo en el puerto:", port));
+
+//Middleware
+app.use(express.json());
+
+//Routes
+app.get('/api/products', () => {
+    //(GET) Mostrar todos los productos
+});
+
+app.get('/api/products/:pid', () => {
+    //(GET) Mostrar producto que coincida con :pid
+});
+
+//Agregar nuevo producto
+app.post('/api/products', () => {
+    //(POST) Crear producto nuevo
+});
+
+//Para poder editar
+app.put('/api/products/:pid', () => {
+    //(PUT) Editar producto que coincida con :pid
+});
+
+app.delete('/api/products/:pid', () => {
+    //(DELETE) Eliminar producto que coincida con :pid
+});
