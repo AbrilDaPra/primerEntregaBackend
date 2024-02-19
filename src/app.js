18a7e11513cdfe4d16a5bdbdcbc83887c04732e4
Abril Da Pra <abydapra@gmail.com>
2024-02-19 14:54:01 -0300

    Comienzo a escribir las rutas

--- src/app.js
+++ src/app.js
@@ -9,24 +9,36 @@ app.listen(port, console.log("El servidor esta corriendo en el puerto:", port));
 app.use(express.json());
 
 //Routes
-app.get('/api/products', () => {
+app.get('/api/products/', () => {
     //(GET) Mostrar todos los productos
 });
 
-app.get('/api/products/:pid', () => {
+app.get('/api/products/:pid/', () => {
     //(GET) Mostrar producto que coincida con :pid
 });
 
 //Agregar nuevo producto
-app.post('/api/products', () => {
+app.post('/api/products/', () => {
     //(POST) Crear producto nuevo
 });
 
 //Para poder editar
-app.put('/api/products/:pid', () => {
+app.put('/api/products/:pid/', () => {
     //(PUT) Editar producto que coincida con :pid
 });
 
-app.delete('/api/products/:pid', () => {
+app.delete('/api/products/:pid/', () => {
     //(DELETE) Eliminar producto que coincida con :pid
+});
+
+app.post('/api/carts/', () => {
+    //(POST) 
+});
+
+app.get('/api/carts/:cid/', () => {
+    //(GET) 
+});
+
+app.post('/api/carts/:cid/product/:pid', () => {
+    //(POST) 
 });
