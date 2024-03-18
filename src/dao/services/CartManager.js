761c6ff9a823616e007abf70b6fabd2e1140ec52
Abril Da Pra <abydapra@gmail.com>
2024-03-17 22:09:27 -0300

    Cambio link mongodb por local

--- src/dao/services/CartManager.js
+++ src/dao/services/CartManager.js
@@ -1,57 +1,80 @@
-import mongoose from 'mongoose';
 import cartsModel from '../models/carts.model.js';
 
-
-
-
-import { randomUUID }  from 'node:crypto';
-
-class CartManager{
+class CartManager {
     constructor(){
-        this.carts = [];
+
     }
 
-    async createCart(){
-        let newCart =  {
-            id: randomUUID(),
-            products: [],
-        };
-        this.carts.push(newCart);
-        return newCart;
+    createCart = async(limit) => {
+        let result = await cartsModel.find().limit(limit);
+        return result;
     }
 
-    async getCartById(cid){
-        const finalCart = this.carts.find(cart => cart.id === cid);
-
-        if(finalCart) {
-            return finalCart.products;
-        } else {
-            throw new Error("Cart not found.");
-        }
+    getCartById = async(id) => {
+        let result = await cartsModel.findById(id);
+        return result;
     }
 
-    async addProductToCart(cid, pid){
-        const cart = this.carts.find(cart => cart.id === cid);
-
-        if(!cart) {
-            throw new Error('Cart not found.')
-        }
-
-        //Verifico si el producto ya existe en el carrito
-        const existingProduct = cart.products.find(product => product.id === pid);
-        if (existingProduct) {
-            //Si ya existe, incremento la cantidad
-            existingProduct.quantity++;
-        } else {
-            //Si no existe, agrego cantidad 1
-            cart.products.push({
-                id: pid,
-                quantity: 1
-            });
-        }
-
-        return "Product added to cart.";
+    addProductToCart = async() => {
+        let result = await cartsModel.find({brand: brand});
+        return result;
     }
 }
 
 export default CartManager;
+
+
+
+// MANAGER DE FS
+
+// import { randomUUID }  from 'node:crypto';
+
+// class CartManager{
+//     constructor(){
+//         this.carts = [];
+//     }
+
+//     async createCart(){
+//         let newCart =  {
+//             id: randomUUID(),
+//             products: [],
+//         };
+//         this.carts.push(newCart);
+//         return newCart;
+//     }
+
+//     async getCartById(cid){
+//         const finalCart = this.carts.find(cart => cart.id === cid);
+
+//         if(finalCart) {
+//             return finalCart.products;
+//         } else {
+//             throw new Error("Cart not found.");
+//         }
+//     }
+
+//     async addProductToCart(cid, pid){
+//         const cart = this.carts.find(cart => cart.id === cid);
+
+//         if(!cart) {
+//             throw new Error('Cart not found.')
+//         }
+
+//         //Verifico si el producto ya existe en el carrito
+//         const existingProduct = cart.products.find(product => product.id === pid);
+//         if (existingProduct) {
+//             //Si ya existe, incremento la cantidad
+//             existingProduct.quantity++;
+//         } else {
+//             //Si no existe, agrego cantidad 1
+//             cart.products.push({
+//                 id: pid,
+//                 quantity: 1
+//             });
+//         }
+
+//         return "Product added to cart.";
+//     }
+// }
+
+// export default CartManager;
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 export const connectDB = async () => {
     try{
-        await connect("mongodb+srv://abydapra:161199@codercluster.4dplmqx.mongodb.net/?retryWrites=true&w=majority&appName=CoderCluster")
+        await connect("mongodb://127.0.0.1:27017/ecommerce?retryWrites=true&w=majority")
         console.log("Connected to MongoDB");
     } catch(error) {
         console.error("Error connecting to MongoDB", error);
