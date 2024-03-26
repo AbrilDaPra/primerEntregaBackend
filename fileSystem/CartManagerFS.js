06d75e95de931136abe813777992293d867ca938
Abril Da Pra <abydapra@gmail.com>
2024-03-26 11:32:11 -0300

    Instalo nueva dependencia y creo carpeta para separar managers con fileSystem

---
+++ fileSystem/CartManagerFS.js
@@ -0,0 +1,51 @@
+import { randomUUID }  from 'node:crypto';
+
+class CartManager{
+    constructor(){
+        this.carts = [];
+    }
+
+    async createCart(){
+        let newCart =  {
+            id: randomUUID(),
+            products: [],
+        };
+        this.carts.push(newCart);
+        return newCart;
+    }
+
+    async getCartById(cid){
+        const finalCart = this.carts.find(cart => cart.id === cid);
+
+        if(finalCart) {
+            return finalCart.products;
+        } else {
+            throw new Error("Cart not found.");
+        }
+    }
+
+    async addProductToCart(cid, pid){
+        const cart = this.carts.find(cart => cart.id === cid);
+
+        if(!cart) {
+            throw new Error('Cart not found.')
+        }
+
+        //Verifico si el producto ya existe en el carrito
+        const existingProduct = cart.products.find(product => product.id === pid);
+        if (existingProduct) {
+            //Si ya existe, incremento la cantidad
+            existingProduct.quantity++;
+        } else {
+            //Si no existe, agrego cantidad 1
+            cart.products.push({
+                id: pid,
+                quantity: 1
+            });
+        }
+
+        return "Product added to cart.";
+    }
+}
+
+export default CartManager;
---
+++ fileSystem/ProductManagerFS.js
@@ -0,0 +1,118 @@
+import { randomUUID }  from 'node:crypto';
+import fs from 'fs/promises';
+
+let products = [];
+class ProductManager {
+    constructor(){
+        this.products = [];
+        this.productsFilePath = 'database/products.json';
+    }
+
+    async loadProductsFromFile() {
+        try{
+            const productsData = await fs.readFile(this.productsFilePath, 'utf-8');
+            this.products = JSON.parse(productsData);
+        } catch (err) {
+            console.error('Error loading products from file:', err);
+        }
+    }
+
+    async saveProductsToFile() {
+        try{
+            await fs.writeFile(this.productsFilePath, JSON.stringify(this.products, null, 2));
+        } catch (err) {
+            console.error('Error saving products to file:', err);
+        }
+    }
+
+    async getProducts() {
+        return this.loadProductsFromFile();
+        return this.products;
+    }
+
+    async getProductsById(productId) {
+        const foundProduct = this.products.find(product => product.id === productId);
+
+        if(foundProduct) {
+            return foundProduct;
+        } else {
+            return null;
+        }
+    }
+
+    async addProduct(product) {
+        try{
+            await this.loadProductsFromFile();
+            if(
+                this.products.some(
+                    (existingProduct) => existingProduct.code === product.code
+                )
+            ) {
+                throw new Error ("A product already exists with that code.");
+            }
+
+            if(
+                !product.title || 
+                !product.description || 
+                !product.code || 
+                !product.price || 
+                !product.status || 
+                !product.stock || 
+                !product.category
+            ) {
+                throw new Error ("All fields must be completed.");
+            }
+
+            const newProduct = {
+                id: randomUUID(),
+                ...product
+            };
+
+            this.products.push(newProduct);
+
+            await this.saveProductsToFile();
+
+            return "The product was added correctly."
+        } catch (err) {
+            throw new Error("There was an error when trying to add the product to the cart.");
+        }
+    }
+
+    async updateProduct(productId, updatedFields){
+        const indexToUpdate = this.products.findIndex(product => product.id === productId);
+
+        if(indexToUpdate !== -1) {
+            Object.assign(this.products[indexToUpdate], updatedFields);
+            await this.saveProductsToFile();
+            console.log("Product updated correctly.")
+        } else {
+            throw new Error("A product with the specified ID was not found.")
+        }
+    }
+
+    async deleteProduct(productId) {
+        try{
+            const indexToDelete = this.products.findIndex(product => product.id === productId);
+
+            if(indexToDelete !== -1) {
+                this.products.splice(indexToDelete, 1);
+                await this.saveProductsToFile();
+                console.log("Product deleted correctly.");
+            } else {
+                console.error("Could not delete because a product with that ID was not found.");
+            } 
+        } catch (err) {
+            console.error("Error when trying to delete the product:", err.message);
+        }
+    }
+}
+
+export const addProduct = (newProduct) => {
+    products.push(newProduct);
+}
+
+export const getAllProducts = () => {
+    return products;
+}
+
+export default ProductManager;
